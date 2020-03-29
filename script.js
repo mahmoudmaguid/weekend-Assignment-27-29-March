6b7e33b4cf58552a3dc862266ff1b6260d112310
Mahmoud Maguid <mahmoud.maguid@yahoo.ca>
2020-03-29 15:27:52 -0600

    v3

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+/*Create a function that takes an array of numbers and return "Boom!" if the number 7 appears in the array. Otherwise, return "there is no 7 in the array".*/
+
    function sevenBoom(insertArrY)/*I create Functiont that takes array as parameter*/ 
    
    
